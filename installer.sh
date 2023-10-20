3d1453dec1d027b23a06e1343864507bcd4fe9de
ididitbad <ididitbad@users.noreply.github.com>
2023-10-21 00:17:14 +0400

    installer: new packages and sysrq enabling

--- installer.sh
+++ installer.sh
@@ -2,14 +2,43 @@
 
 ################ INSTALLER ####################################################
 
-pacman -S --noconfirm base-devel git stow vim zsh ttf-ubuntu-font-family alacritty tmux byobu telegram-desktop unzip openssh
+pkgs=(
+    base-devel
+    git
+    stow
+    ripgrep
+    the_silver_searcher
+    fd
+    fzf
+    vim
+    zsh
+    ttf-ubuntu-font-family
+    alacritty
+    tmux
+    byobu
+    telegram-desktop
+    unzip
+    openssh
+    thefuck
+    bat
+)
+
+aurs=(
+    spotify
+    universal-ctags-git
+)
+
+pacman -S --noconfirm ${pkgs[*]}
 
 git clone https://aur.archlinux.org/yay.git /tmp/yay && cd $_
 makepkg -si --noconfirm
 
-yay -S --noconfirm spotify
+yay -S --noconfirm ${aurs[*]}
 
 systemctl enable sshd.service
 systemctl start sshd.service
 
 chsh -s $(which zsh)
+
+tee -a /etc/sysctl.d/99-sysctl.conf <<< 'kernel.sysrq = 1'
+sysctl kernel.sysrq=1
